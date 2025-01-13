da2ff16fe2e8209820eb89835e6ab6cf30ab5ddc
Glenn Brown <glenn.brown@gmail.com>
2025-01-13 18:42:48 -0500

    Added nvm

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -76,3 +76,6 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 zstyle ':completion:*' keep-prefix true
 
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+# NVM Completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
